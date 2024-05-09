521eaf4049b2f1942f20c1598d77dd2bc15d3119
Kollin Trujillo <kollintrujillo@live.com>
2024-05-09 15:27:22 -0700

    Updated layout

--- README.md
+++ README.md
@@ -3,7 +3,21 @@
 This project stemmed from exploring algorithms that are utilized for solving Graph Isomorphism problems. I chose to focus on the Graph-Subgraph Isomorphism problem and wanted to explore how to use graph algorithms to determine the largest common substructures between graphs.
 
 ## Motivation
-Molecules are ubiquitous. They make up drugs, materials, and many more.
+Molecules are ubiquitous. They make up drugs, materials, and many more. 
+
+Primary motivation is due to a medicinal chemistry course that was taken and understanding if there was a graph-theoretic approach to determining pharmacophores for pairs or larger of molecules.
 
 ## Background on Algorithm
-The VF2 algorithm comes from the paper "An Improved Algorithm for Matching Large Graphs" written by L. P. Cordella, P. Foggia, C. Sansone, and M. Vento. The idea behind
+The VF2 algorithm comes from the paper "An Improved Algorithm for Matching Large Graphs" written by L. P. Cordella, P. Foggia, C. Sansone, and M. Vento. The idea behind the algorithm is shown in the following figure taken from the paper.
+
+![VF2Algo](C:\Users\kolli\Desktop\VF2Algo\VF2-Molecular-Similarity-Checker\images\VF2Algo.png)
+
+Which shows us that this Match algorithm scans for common matches and grows the subgraph and if not include then includes backtracking  to help determine the best most connected graph.
+
+## Implementation
+
+The implementation was carried out in Python referencing some packages such as RDKit.
+
+## TODO
+
+\- [ ] Clean up the Jupyter Notebook for examples of utilization beyond the main script
--- main.py
+++ main.py
@@ -1,9 +1,55 @@
-from rdkit import Chem
-from rdkit.Chem import Draw
+from VF2 import VF2Matcher, molecule_to_graph, print_matched_subgraph, drawCommonMolecule, benchmark_molecules
 
-# Example molecule: Aspirin
-aspirin_smiles = "CC(=O)OC1=CC=CC=C1C(=O)O"
-molecule = Chem.MolFromSmiles(aspirin_smiles)
+ethanol = Chem.MolFromSmiles("CCO")
+methanol = Chem.MolFromSmiles("CO")
+ethanol_graph = molecule_to_graph(ethanol)
+methanol_graph = molecule_to_graph(methanol)
 
-# Visualize the molecule
-Draw.MolToImage(molecule)
+matcher = VF2Matcher(ethanol_graph, methanol_graph)
+
+# TODO: Maybe refactor to choose the larger of the two molecules and grab that name for the file?
+drawCommonMolecule(matcher, ethanol, "ethanol")
+drawCommonMolecule(matcher, methanol, "methanol")
+
+# Time the ones below
+# Assuming you have defined ethanol and methanol using RDKit
+ibuprofen = Chem.MolFromSmiles("CC(C)CC1=CC=C(C=C1)C(C)C(=O)O")
+ibuprofen_graph = molecule_to_graph(ibuprofen)
+naproxen = Chem.MolFromSmiles("CC(C1=CC2=C(C=C1)C=C(C=C2)OC)C(=O)O")
+naproxen_graph = molecule_to_graph(naproxen)
+
+matcher = VF2Matcher(ibuprofen_graph, naproxen_graph)
+match = matcher.is_match()
+
+print("Is there a match:", match)
+
+matcher.print_largest_match()  # Print the largest common subgraph
+print_matched_subgraph(matcher)
+
+matched_atoms_ibuprofen_naproxen = list(matcher.largest_match.keys())
+
+# Highlight these atoms in the ethanol molecule
+img = Draw.MolToImage(ibuprofen, highlightAtoms=matched_atoms_ibuprofen_naproxen, size=(300, 300))
+drawCommonMolecule(matcher, ibuprofen, "ibuprofen")
+drawCommonMolecule(matcher, naproxen, "naproxen")
+
+benzene = Chem.MolFromSmiles("C1=CC=CC=C1")
+benzene_graph = molecule_to_graph(benzene)
+napthalene = Chem.MolFromSmiles("C1=CC=C2C=CC=CC2=C1")
+napthalene_graph = molecule_to_graph(napthalene)
+
+indole = Chem.MolFromSmiles("C1=CC=C2C(=C1)C=CN2")
+indole_graph = molecule_to_graph(indole)
+tryptophan = Chem.MolFromSmiles("C1=CC=C2C(=C1)C(=CN2)CC(C(=O)O)N")
+tryptophan_graph = molecule_to_graph(tryptophan)
+
+acetic_acid = Chem.MolFromSmiles("CC(=O)O")
+acetic_acid_graph = molecule_to_graph(acetic_acid)
+stearic_acid = Chem.MolFromSmiles("CCCCCCCCCCCCCCCCCC(=O)O")
+stearic_acid_graph = molecule_to_graph(stearic_acid)
+
+# (benzene_graph, "benzene", napthalene_graph, "napthalene"), (indole_graph, "indole", tryptophan_graph, "tryptophan"), (acetic_acid_graph, "acetic_acid", tryptophan_graph, "tryptophan")
+
+
+molecule_pairs = [(methanol, methanol_graph, "methanol", ethanol, ethanol_graph, "ethanol"), (ibuprofen, ibuprofen_graph, "ibuprofen", naproxen,naproxen_graph, "naproxen"), (benzene, benzene_graph, "benzene", napthalene, napthalene_graph, "napthalene"), (indole, indole_graph, "indole", tryptophan, tryptophan_graph, "tryptophan"), (acetic_acid, acetic_acid_graph, "acetic_acid", stearic_acid, stearic_acid_graph, "stearic_acid")]
+benchmark_molecules(molecule_pairs)
